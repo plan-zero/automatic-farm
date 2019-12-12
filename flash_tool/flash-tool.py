e75ef67e82ea39795bc4bbc52f34c7ec59408445
unknown <mestereaga.andrei@gmail.com>
2019-12-12 15:09:57 +0200

    Add command line support to flashing tool

--- flash_tool/flash-tool.py
+++ flash_tool/flash-tool.py
@@ -1,6 +1,9 @@
 import serial.tools.list_ports
 import serial, time
 import sys
+import os
+
+VERSION = "1.0.0"
 
 comPorts = []
 hexFileData = []
@@ -295,7 +298,7 @@ def flash_data(state):
 				state = 99
 
 
-def main():	
+def main(argv):	
 
 	# TODO:4
 	# 	Option 2: Open COM port selected (not hardcoded)
@@ -330,6 +333,55 @@ def main():
 			exit()
 
 			
+help_string = """
+-P : serial port, e.g -P COM1
+-B : serial baudrate, the value must be in 0-250K interval
+-H : HEX path, use the absolute path of file
+-T : target address, the value must be less or equal to 5 e.g ABCDE
+-h : help, prints this message
+--v : tool version
+-V : verbose mode, prints debugging messages during script execution """
+_commands = {
+	"-P" : ["",		lambda x: "COM" == x[:3] and x[3:].isdigit(),		"-P: The supported port is COMx where x must be an unsigned int"],
+	"-B" : ["",     lambda x: int(x,10) > 0 and int(x,10) <= 250000, 	"-B: The baud rate should be in 0-250000 range" ],
+	"-H" : ["", 	lambda x: x != "" and os.path.isfile(x), 			"-H: File not found"],
+	"-T" : ["",		lambda x: x != "" and len(x) <= 5,					"-T: The length of TX/RX address must be less or equal to 5"],
+	"-h" : ["-",	lambda x: True,										help_string],
+	"--v": ["-",	lambda x: True,                                    	"--v: Tool version is: " + VERSION],
+	"-V" : ["-",	lambda x: False,									"-V: Run in verbose mode"]
+}			
 
 if __name__== "__main__":
-  	main()
+    command_err = 0
+    print(sys.argv)
+    for arg in sys.argv:
+        if arg in _commands:
+            try:
+                if _commands[arg][0] == "-":
+                    if _commands[arg][1](0) == True:
+                        print(_commands[arg][2])
+                    _commands[arg][0] = "*"
+                else:
+                    idx = sys.argv.index(arg)
+                    value = sys.argv[idx + 1]
+                    if _commands[arg][1](value) == False:
+                        print(_commands[arg][2])
+                        command_err = 1
+                    else:
+                        _commands[arg][0] = value
+            except Exception as e:
+                print(_commands[arg][0] + ":command error, use -h to see the correct usage")
+                command_err = 1
+                print(e)
+	
+    #print(_commands)
+    for cmd in _commands:
+        if _commands[cmd][0] == "":
+            print("The " + cmd + " parameter is mandatory!")
+            command_err = 1
+
+    if command_err == 0:
+        main(_commands)
+    else:
+        print("Invalid command, please use -h to see the correct usage")
+
