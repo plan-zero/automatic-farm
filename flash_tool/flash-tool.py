03293b6bf079e31c35990c3d49651b7ae9b00e72
unknown <mestereaga.andrei@gmail.com>
2020-01-14 21:16:54 +0200

    Remove 2x UART speed - this doesn't work well with old quartz, also update UART's baud const values. Change script to use 2 stopbit instead of one (also changed in uart driver)

--- flash_tool/flash-tool.py
+++ flash_tool/flash-tool.py
@@ -69,10 +69,8 @@ def connect_to_com_port(comPort, baud):
 	ser.baudrate = baud
 	ser.bytesize = serial.EIGHTBITS #number of bits per bytes
 	ser.parity = serial.PARITY_NONE #set parity check: no parity
-	ser.stopbits = serial.STOPBITS_ONE #number of stop bits
-	#ser.timeout = None          #block read
-	ser.timeout = 1            #non-block read
-	#ser.timeout = 2              #timeout block read
+	ser.stopbits = serial.STOPBITS_TWO #number of stop bits
+	ser.timeout = None          #block read
 	ser.xonxoff = False     #disable software flow control
 	ser.rtscts = False     #disable hardware (RTS/CTS) flow control
 	ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
