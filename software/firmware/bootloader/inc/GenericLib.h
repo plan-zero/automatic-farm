17710ff7bdb7e499132b21b14e7a40f651aa9158
unknown <mestereaga.andrei@gmail.com>
2020-02-07 22:41:52 +0200

    Add the first version of CMake config

---
+++ software/firmware/bootloader/inc/GenericLib.h
@@ -0,0 +1,34 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 11/28/2019 11:59:14 PM
+ *  Author: Adi
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+#define F_CPU 4000000UL
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/boot.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "E2P_Layout.h"
+#define LED_PORT_DIR DDRB
+#define LED_PORT_PIN PINB0
+
+#define TURN_LED_ON		PORTB |= 1
+#define TURN_LED_OFF	PORTB &= 0xFE
+#define TOGGLE_LED		PORTB ^= 1
+
+#define APP_ADDR_START	0
+#define APP_ADDR_END		0x2800
+
+
+#endif /* INCFILE1_H_ */
---
+++ software/firmware/bootloader/src/main.c
@@ -0,0 +1,56 @@
+/*
+ * bootloader_m16.c
+ *
+ * Created: 11/4/2019 7:34:04 PM
+ * Author : AR
+ */ 
+#include "WriteApp.h"
+#include "GenericLib.h"
+
+#define LED_PORT_DIR DDRB
+#define LED_PORT_PIN PINB0
+
+#define TURN_LED_ON		PORTB |= 1
+#define TURN_LED_OFF	PORTB &= 0xFE
+#define TOGGLE_LED		PORTB ^= 1
+
+int main(void)
+{
+	// Wait after power on in order to stabilize the tension
+	_delay_ms(50);
+	LED_PORT_DIR |= 1 << LED_PORT_PIN;
+
+	uint8_t rx_address[RADIO_RX_LENGTH] = {0};
+	/*uint32_t eep_checksum = 0;
+	uint32_t flash_checksum = 0;
+	uint16_t nrf24_firmware_version = 0;
+	uint16_t application_version = 0;
+	uint16_t eep_version = 0;
+	//uint16_t reserved = 0;*/
+	uint8_t download = 0;
+	uint8_t tx_address[RADIO_RX_LENGTH] = {0};
+	
+	//reading eeprom parameters
+	
+	eeprom_read_block ((void*)&rx_address,				(void*)RADIO_RX_ADDRESS,			RADIO_RX_LENGTH);
+	/*eeprom_read_block ((void*)&eep_checksum,			(void*)E2P_CKS_ADDRESS,				E2P_CKS_LENGTH);
+	eeprom_read_block ((void*)&flash_checksum,			(void*)FLASH_CKS_ADDRESS,			FLASH_CKS_LENGTH);
+	eeprom_read_block ((void*)&nrf24_firmware_version,	(void*)NRF_VERS_ADDRESS,			NRF_VERS_LENGTH);
+	eeprom_read_block ((void*)&application_version,		(void*)APP_VERS_ADDRESS,			APP_VERS_LENGTH);
+	eeprom_read_block ((void*)&eep_version,				(void*)E2P_VERS_ADDRESS,			E2P_VERS_LENGTH);*/
+	eeprom_read_block ((void*)&download,				(void*)DOWNLOAD_FLAG_ADDRESS,		DOWNLOAD_FLAG_LENGTH);
+	eeprom_read_block ((void*)&tx_address,				(void*)PROGRAMMER_ADDR_ADDRESS,		PROGRAMMER_ADDR_LENGTH);
+
+		
+	if( download == 0xAA) {
+		startFlash(rx_address);
+	}
+	
+	/*while(bootloader_state == BOOTLOADER_CHECK_APP)
+	{
+		TURN_LED_OFF;
+	}*/
+	//go to the application as soon as the code has downloaded
+	asm("JMP 0");
+}
+
