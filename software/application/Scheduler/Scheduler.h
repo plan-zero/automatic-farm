2c208be23f12f605a273fb11e9d913f012fc898b
adrianteban <teban.adrian@yahoo.com>
2020-02-10 21:55:37 +0200

    scheduler

--- software/application/Scheduler/Scheduler.h
+++ software/application/Scheduler/Scheduler.h
@@ -8,6 +8,7 @@
 #ifndef SCHEDULER_SCHEDULER_H_
 #define SCHEDULER_SCHEDULER_H_
 
-
+typedef void (*functiontype)(void);
+extern functiontype getPointerTo1msTask(void);
 
 #endif /* SCHEDULER_SCHEDULER_H_ */
