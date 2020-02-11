d4e8de136c3531a50e1f3b00c65319c2a54bcd82
adrianteban <teban.adrian@yahoo.com>
2020-02-12 00:41:04 +0200

    Task scheduler

--- software/application/Abstractization/Sensors/Humidity/Humidity.c
+++ software/application/Abstractization/Sensors/Humidity/Humidity.c
@@ -6,3 +6,7 @@
  */
 
 
+void HUM_Acquire()
+{
+    
+}
--- software/application/Scheduler/Scheduler.c
+++ software/application/Scheduler/Scheduler.c
@@ -5,35 +5,58 @@
  *      Author: Adi
  */
 #include "Scheduler.h"
+#include "Humidity.h"
 
+#define TASK_NUMBER_1MS 	((char)1)
+#define TASK_NUMBER_10MS 	((char)0)
+#define TASK_NUMBER_100MS 	((char)0)
+#define TASK_NUMBER_1S 		((char)0)
+
+
+static volatile char taskFinished = 1;
+unsigned short taskCounter = 0;
+
+voidFunctionType table_task_1ms     [TASK_NUMBER_1MS]   = 
+{
+    HUM_Acquire
+};
+voidFunctionType table_task_10ms    [TASK_NUMBER_10MS]  = { };
+voidFunctionType table_task_100ms   [TASK_NUMBER_100MS] = { };
+voidFunctionType table_task_1s      [TASK_NUMBER_1S]    = { };
 
 void task_1ms();
 
 voidFunctionType getPointerTo1msTask()
 {
-	return task_1ms;
+    return task_1ms;
 }
 
 
 void task_1ms()
 {
+    if(taskFinished)
+    {
+        taskFinished = 0;
 
+        taskCounter++;
+    
+        for (int taskIterator = 0; taskIterator < TASK_NUMBER_1MS; taskIterator++)
+        {
+            table_task_1ms[taskIterator]();
+        }
+        
+
+        
+
+        taskFinished = 1;
+    }
+    else
+    {
+        //TODO: WDG reset
+    }
+    
 }
 
-void task_10ms()
-{
-
-}
-
-void task_100ms()
-{
-
-}
-
-void task_1s()
-{
-
-}
 
 
 
