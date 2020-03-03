05165be49d5f61d3098cf6085770bc062a0bb878
andreimestereaga <mestereaga.andrei@gmail.com>
2020-03-03 11:22:30 +0200

    Add license header for all files (#23)

--- software/CheckArguments.cmake
+++ software/CheckArguments.cmake
@@ -1,3 +1,20 @@
+# Copyright (C) 2020 Coding Night Romania
+# 
+# This file is part of automatic-farm.
+# 
+# automatic-farm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# automatic-farm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with automatic-farm.  If not, see <http://www.gnu.org/licenses/>.
+
 #when a new hardware is added, please update these strings as well
 set(SUPPORTED_MCU "atmega16 atmega328p")
 set(SUPPORTED_FREQ "1000000UL 2000000UL 4000000UL 8000000UL 16000000UL")
