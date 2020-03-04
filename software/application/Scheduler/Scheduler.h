aacdd654e932a769cd0cc0ee056ff200d210781e
amestereaga <mestereaga.andrei@gmail.com>
2020-03-04 17:24:46 +0200

    Modify the scheduler to allow to add tasks at runtime

--- software/application/Scheduler/Scheduler.h
+++ software/application/Scheduler/Scheduler.h
@@ -8,7 +8,14 @@
 #ifndef SCHEDULER_SCHEDULER_H_
 #define SCHEDULER_SCHEDULER_H_
 
-typedef void (*voidFunctionType)(int);
+typedef enum{
+    sch_type_task_1ms,
+    sch_type_task_10ms,
+    sch_type_task_100ms,
+    sch_type_task_1s,
+}scheduler_task_type;
+
+//typedef void (*voidFunctionType)(int);
 typedef void (*voidFunctionTypeVoid)();
 extern voidFunctionTypeVoid getPointerTo1msTask(void);
 
