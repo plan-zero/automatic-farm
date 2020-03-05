a2fdfcc1736588488649d0b698bfe2d0f0b5d980
Romina <rominabaic23@gmail.com>
2020-03-05 19:52:48 +0200

    testing functTimer

--- software/testing/timer_testbench/timer_test.c
+++ software/testing/timer_testbench/timer_test.c
@@ -25,6 +25,49 @@
 typedef enum{
     funct_timer_mode_normal_mode,
     funct_timer_mode_pwm,
+    funct_timer_mode_ctc,
+    funct_timer_fast_pwm,
+    funct_timer_set_ovf_int,
+    funct_timer_get_ovf_int,
+    funct_timer_set_ocmp_int,
+    funct_timer_get_ocmp_int,
+    funct_timer_clear_ovf_int_flag,
+    funct_timer_get_ovf_int_flag,
+    funct_timer_clear_ocmp_int_flag,
+    funct_timer_get_ocmp_init_flag,
+    funct_timer_normal_operation,
+    funct_timer_toggle_cmp_match,
+    funct_timer_clear_cmp_match,
+    funct_timer_set_cmp_match,
+    funct_timer_clock_disabled,
+    funct_timer_clock_pre_1,
+    funct_timer_clock_pre_8,
+    funct_timer_clock_pre_64,
+    funct_timer_clock_pre_256,
+    funct_timer_clock_pre_1024,
+    funct_timer_clock_pre_T0RE,
+    funct_timer_mode_pwm_8bit,
+    funct_timer_mode_pwm_9bit,
+    funct_timer_mode_10bit,
+    funct_timer_mode_ctc_1,
+    funct_timer_mode_fastpwm_8bit,
+    funct_timer_mode_fastpwm_9bit,
+    funct_timer_mode_fastpwm_10bit,
+    funct_timer_mode_pwm_pfc1,
+    funct_timer_mode_pwm_pfc2,
+    funct_timer_mode_pwm_pc1,
+    funct_timer_mode_pwm_pc2,
+    funct_timer_mode_ctc_2,
+    funct_timer_mode_fastpwm_1,
+    funct_timer_mode_fastpwm_2,
+    funct_timer_mode_set_ic_int,
+    funct_timer_mode_get_ic_int,
+    funct_timer_get_output_cmp,
+    funct_timer_set_output_cmp,
+    funct_timer_get_input_capture,
+    funct_timer_set_input_capture,
+
+
 }funct_timer;
 
 void exec_test(funct_timer funct, uint8_t inst, uint8_t reg_initial, uint8_t expected, volatile uint8_t * reg)
@@ -42,6 +85,42 @@ void exec_test(funct_timer funct, uint8_t inst, uint8_t reg_initial, uint8_t exp
         uart_printString("Execute MODE_PWM",1);
         TIMER_FUNCTIONS(MODE_PWM, inst);
         break;
+    case funct_timer_mode_ctc:
+        uart_printString("Execute MODE_PWM",1);
+        TIMER_FUNCTIONS(MODE_CTC, inst);
+        break;   
+    case funct_timer_fast_pwm:
+        uart_printString("Execute MODE_FAST_PWM",1);
+        TIMER_FUNCTIONS(MODE_FAST_PWM, inst);
+        break;
+    case funct_timer_set_ovf_int:
+        uart_printString("Execute SET_OVF_INT",1);
+        TIMER_FUNCTIONS(SET_OVF_INT, inst);
+        break;
+    case funct_timer_get_ovf_int:
+        uart_printString("Execute GET_OVF_INT",1);
+        TIMER_FUNCTIONS(GET_OVF_INT, inst);
+        break;
+    case funct_timer_set_ocmp_int:
+        uart_printString("Execute SET_OCMP_INT",1);
+        TIMER_FUNCTIONS(SET_OCMP_INT, inst, TIMER_CH_A);
+        break;
+    case funct_timer_get_ocmp_int:
+        uart_printString("Execute GET_OCMP_INT",1);
+        TIMER_FUNCTIONS(GET_OCMP_INT, inst, TIMER_CH_A);
+        break;
+    case funct_timer_clear_ovf_int_flag:
+        uart_printString("Execute CLEAR_OVF_INT_FLAG",1);
+        TIMER_FUNCTIONS(CLEAR_OVF_INT_FLAG, inst);
+        break;
+    case funct_timer_get_ovf_int_flag:
+        uart_printString("Execute GET_OVF_INT_FLAG",1);
+        TIMER_FUNCTIONS(GET_OVF_INT_FLAG, inst);
+        break;
+    case funct_timer_clear_ocmp_int_flag:
+        uart_printString("Execute CLEAR_OCMP_INT_FLAG",1);
+        TIMER_FUNCTIONS(CLEAR_OCMP_INT_FLAG, inst, TIMER_CH_A);
+        break; 
     default:
         break;
     }
