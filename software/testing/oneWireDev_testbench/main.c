7a3ed14ac805f6b9665d79fa22e4fcba7989aadd
amestereaga <mestereaga.andrei@gmail.com>
2021-02-07 23:58:46 +0200

    Add code for ds18b20 sensor

--- software/testing/oneWireDev_testbench/main.c
+++ software/testing/oneWireDev_testbench/main.c
@@ -1,51 +1,30 @@
 #include <avr/io.h>
 #include "uart.h"
 
-
+#include "ds18b20.h"
 #include <util/delay.h>
 #include "stdint.h"
+#include "stdio.h"
 
-
-
-//0 logic ii 50 ms pe low si 1 logic 2ms pe low in rest pe high
-
-void sendData(uint8_t data)
-{
-    uint8_t bit_data = 0;
-    for(int i=7; i>=0; i--)
-    {
-        PORTB |=0x08;
-        _delay_ms(1);
-        
-        bit_data = (data>>i) & 0x01;
-        if(bit_data == 1)
-        {
-            PORTB &= 0xF7;
-            _delay_ms(10);
-        }
-        else
-        {
-            PORTB &=0xF7;
-            _delay_ms(2);
-        }
-    }
-    PORTB |=0x08;
-}
 
 
 int main()
 {
 
-    DDRD |= 0x08;
+    uart_init(UART_115200BAUD, UART_110592MHZ, UART_PARITY_NONE);
+    uart_printString("One wire testing", 1);
 
+    char string[20];
+    uint32_t data = 0;
 
-
-    uart_init(UART_9600BAUD, UART_110592MHZ, UART_PARITY_NONE);
-    uart_printString("One wire setup", 1);
     while(1)
     {
         //sendData(75);
+        data = ds18b20_gettemp();
+        sprintf(string,"TMP:%u", (int)data);
+        uart_printString(string,1);
         PORTD ^= 0x08;
-        _delay_ms(500);
+        _delay_ms(1000);
+
     }
 }
